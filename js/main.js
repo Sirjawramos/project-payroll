a99fa386e0bc0b41265876120879a2705410d4bb
CodeVader <victor.chike@yahoo.com>
2019-05-31 04:56:27 +0100

    Create Employee Add Function

--- js/main.js
+++ js/main.js
@@ -23,4 +23,51 @@ $(function(){
 
     });
 
+
+    //Create Button Function to create new Employee
+    $('#btn-create').on('click', function(event){
+        event.preventDefault();
+        var name = $('#name').val();
+        var address = $('#address').val();
+        var role = $('#role').val();
+        var gender = $('#gender').val();
+        var phone = $('#phone').val();
+        var email = $('#email').val();
+        var level = $('#level').val();
+        var qualification = $('#qualification').val();
+
+
+        var newEmployee = {
+            name: name,
+            address: address,
+            role: role,
+            gender: gender,
+            phone: phone,
+            email: email,
+            level:  level,
+            qualification: qualification,
+            paid: "No"
+
+        };
+
+        console.log(newEmployee);
+
+        //Post Method Being Applied
+        $.ajax({
+            method: "POST",
+            url: site,
+            data: newEmployee,
+            cache: false,
+            success: function(){
+                alert("New Employee has been registered!");
+                $('#myform').get(0).reset();
+
+            },
+            error: function(){
+                alert("Failed to create");
+            }
+        });
+
+    });
+
 })
